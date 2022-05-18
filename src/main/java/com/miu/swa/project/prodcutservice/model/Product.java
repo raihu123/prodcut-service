c1c5ee8466ea70fdee63cfa73ad72b37d864e28f
rayhanrashid <12345678asdf>
2022-05-18 18:04:08 -0500

    added Product

--- src/main/java/com/miu/swa/project/prodcutservice/model/Product.java
+++ src/main/java/com/miu/swa/project/prodcutservice/model/Product.java
@@ -3,8 +3,20 @@ package com.miu.swa.project.prodcutservice.model;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
-@Getter @Setter @AllArgsConstructor
+import java.math.BigInteger;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Document
 public class Product {
-
+    @Id
+    BigInteger prod_num;
+    String name;
+    Double price;
+    String description;
+    BigInteger stock;
 }
