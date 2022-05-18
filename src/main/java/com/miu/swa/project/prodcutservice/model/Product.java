d434754cdfeb61d3fcf1a52f063a692cac3fef6b
rayhanrashid <12345678asdf>
2022-05-18 18:00:32 -0500

    multiple profiling done

---
+++ src/main/java/com/miu/swa/project/prodcutservice/model/Product.java
@@ -0,0 +1,10 @@
+package com.miu.swa.project.prodcutservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter @AllArgsConstructor
+public class Product {
+
+}
